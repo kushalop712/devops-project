1f2fc0fb706b97167f4a49ebae0a077b6676a339
Kushal <kushalsk27@gmail.com>
2026-04-11 12:32:50 +0530

    fixed sonar secutiy issue

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return jsonify({
         "message": "Hello from Secure DevOps Pipeline!",
         "status": "running"
     })
 
-@app.route('/health')
+@app.route('/health', methods=['GET'])
 def health():
     return jsonify({
         "status": "ok"
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='127.0.0.1', port=5000)  # fixed security issue!
